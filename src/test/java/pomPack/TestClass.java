c7ab6de5a836a5690c03387a078606c3c18aa4cc
800709644 <800709644@IMS-HP240G9-012.imsplgroup.in>
2025-02-25 13:32:59 +0530

    pushing new added code avinash

--- src/test/java/pomPack/TestClass.java
+++ src/test/java/pomPack/TestClass.java
@@ -1,6 +1,9 @@
 package pomPack;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,10 +14,16 @@ import org.testng.annotations.Test;
 
 public class TestClass {
 
+	public Properties p;
 	WebDriver driver;
 	@BeforeClass
-	public void setup() throws InterruptedException
+	public void setup() throws InterruptedException, IOException
 	{
+		
+		FileReader file=new FileReader("/seleniumWebdriver/src/test/resources/config.properties");
+		p=new Properties();
+		p.load(file);
+		
 		driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
